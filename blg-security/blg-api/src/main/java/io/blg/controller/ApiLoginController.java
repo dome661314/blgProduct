6068bd00c1c8c2025c44d5c2346a6fc4816848da
郑伟 <dome@zhengweideMacBook-Pro.local>
2018-12-19 19:09:04 +0800

    first commit

---
+++ blg-security/blg-api/src/main/java/io/blg/controller/ApiLoginController.java
@@ -0,0 +1,55 @@
+package io.blg.controller;
+
+
+import io.blg.annotation.Login;
+import io.blg.common.utils.R;
+import io.blg.common.validator.ValidatorUtils;
+import io.blg.form.LoginForm;
+import io.blg.service.TokenService;
+import io.blg.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.Map;
+
+/**
+ * 登录接口
+ *
+ * @author zhengwei
+ * @email sunlightcs@gmail.com
+ * @date 2018-12-23 15:31
+ */
+@RestController
+@RequestMapping("/api")
+@Api(tags="登录接口")
+public class ApiLoginController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private TokenService tokenService;
+
+
+    @PostMapping("login")
+    @ApiOperation("登录")
+    public R login(@RequestBody LoginForm form){
+        //表单校验
+        ValidatorUtils.validateEntity(form);
+
+        //用户登录
+        Map<String, Object> map = userService.login(form);
+
+        return R.ok(map);
+    }
+
+    @Login
+    @PostMapping("logout")
+    @ApiOperation("退出")
+    public R logout(@ApiIgnore @RequestAttribute("userId") long userId){
+        tokenService.expireToken(userId);
+        return R.ok();
+    }
+
+}
